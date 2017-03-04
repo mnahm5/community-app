33b52fab649db5279764a4e7e876e1c5c41085de
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-03-04 23:09:10 +1100

    can add community image

--- CommunityApp/src/main/java/com/mnahm5/community/CommunityDetailsActivity.java
+++ CommunityApp/src/main/java/com/mnahm5/community/CommunityDetailsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
@@ -19,11 +20,19 @@ import android.widget.Toast;
 
 import com.joanzapata.iconify.Iconify;
 import com.joanzapata.iconify.fonts.FontAwesomeModule;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.parse.starter.R;
 
 import org.w3c.dom.Text;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommunityDetailsActivity extends AppCompatActivity {
 
@@ -103,6 +112,47 @@ public class CommunityDetailsActivity extends AppCompatActivity {
 
     public void AddCommunity(View view)
     {
+        Bitmap image = ((BitmapDrawable) ivProfilePic.getDrawable()).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        ParseFile file = new ParseFile("image.png", byteArray);
 
+        final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText etDescription = (EditText) findViewById(R.id.etDescription);
+
+        List<String> adminList = new ArrayList<>();
+        adminList.add(ParseUser.getCurrentUser().getUsername());
+        List<String> followingList = new ArrayList<>();
+        followingList.add(ParseUser.getCurrentUser().getUsername());
+
+        ParseObject community = new ParseObject("Community");
+        community.put("name", etName.getText().toString());
+        community.put("description", etDescription.getText().toString());
+        community.put("image", file);
+        community.put("admins", adminList);
+        community.put("following", followingList);
+
+        community.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Community Created",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                    Intent intent = new Intent(getApplicationContext(), CommunitiesListActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            e.getMessage(),
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+            }
+        });
     }
 }
